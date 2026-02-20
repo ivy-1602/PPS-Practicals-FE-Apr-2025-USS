f6b7da54f98a04cb160099153ac5073689547638
Ivy <uma2007salunke@gmail.com>
2026-02-20 16:52:13 +0530

    Create even_odd_classic.py

---
+++ even_odd_classic.py
@@ -0,0 +1,10 @@
+"""
+Even or Odd Number Checker
+Simple modulo operation to determine if a number is divisible by 2.
+"""
+
+num = int(input("Enter the number: "))
+if (num % 2 == 0):
+    print("number is even")
+else:
+    print("number is odd")
